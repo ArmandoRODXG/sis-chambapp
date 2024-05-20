8aa8e5186b17cb9a268e23092ee59420b6381d47
Brayan Bautista <76666927+bryanBH1@users.noreply.github.com>
2024-05-20 14:45:49 -0600

    A la espera de parametros

--- app/src/main/java/com/project/chambaapp/activities/WorkerViews/ProfileJobActivity.kt
+++ app/src/main/java/com/project/chambaapp/activities/WorkerViews/ProfileJobActivity.kt
@@ -3,10 +3,33 @@ package com.project.chambaapp.activities.WorkerViews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.chambaapp.R
+import com.project.chambaapp.databinding.ActivityProfileJobBinding
+
 
 class ProfileJobActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileJobBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_job)
+        binding = ActivityProfileJobBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    /* 
+    val usuarioContratista = intent.getStringExtra("nombreContratista")
+    val correoContratista = intent.getStringExtra("correoContratista")
+    val contraseñaContratista = intent.getStringExtra("contraseñaContratista")
+    val especialidadContratista = intent.getStringExtra("especialidadContratista")
+    val numeroContratista = intent.getStringExtra("numeroContratista")
+    val cajaServicios = intent.getStringExtra("cajaServicios")
+
+    binding.nombreUsuario.text = usuarioContratista.toString()
+    binding.correoContratista.text = correoContratista.toString()
+    binding.contraseñaContratista.text = contraseñaContratista.toString()
+    binding.especialidadContratista.text = especialidadContratista.toString()
+    binding.numeroContratista.text = numeroContratista.toString()
+    binding.cajaServicios.text = cajaServicios.toString()
+    */
+
+
 }
