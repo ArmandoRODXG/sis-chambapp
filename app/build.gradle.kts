fc74ad57301b761b4bef18b9bc17161c2c6cf014
JoseAGarciaA87mx <jose.garcia0857@alumnos.udg.mx>
2024-05-21 15:08:28 -0600

    Push Forzado 2do Intento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,10 +42,13 @@ android {
     viewBinding{
         enable = true
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0") //
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -53,6 +56,10 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     kapt("androidx.room:room-compiler:2.6.1")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
@@ -68,6 +75,7 @@ dependencies {
 
     // Firebase Firestore
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-firestore")
 
--- app/src/main/java/com/project/chambaapp/activities/WorkerViews/Paymethodactivity.kt
+++ app/src/main/java/com/project/chambaapp/activities/WorkerViews/Paymethodactivity.kt
@@ -3,19 +3,24 @@ package com.project.chambaapp.activities.WorkerViews
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.project.chambaapp.R
+
+import android.content.Intent
+import com.project.chambaapp.databinding.ActivityPayMethodBinding
 
 class PayMethodActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_pay_method)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+
+    private lateinit var binding: ActivityPayMethodBinding
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                binding = ActivityPayMethodBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                binding.button2.setOnClickListener {
+                    val intent = Intent(this@PayMethodActivity, PaidActivity::class.java).apply {
+
+                    }
+                    startActivity(intent)
+                }
+
+            }
 }
