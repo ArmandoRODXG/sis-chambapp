578883a1894477dcdc6113ad36d24830249c2068
JoseAGarciaA87mx <jose.garcia0857@alumnos.udg.mx>
2024-05-21 14:35:19 -0600

    Push Forzado

--- app/src/main/java/com/project/chambaapp/activities/WorkerViews/Paymethodactivity.kt
+++ app/src/main/java/com/project/chambaapp/activities/WorkerViews/Paymethodactivity.kt
@@ -3,24 +3,19 @@ package com.project.chambaapp.activities.WorkerViews
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
-import android.content.Intent
-import com.project.chambaapp.databinding.ActivityPayMethodBinding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.chambaapp.R
 
 class PayMethodActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityPayMethodBinding
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                binding = ActivityPayMethodBinding.inflate(layoutInflater)
-                setContentView(binding.root)
-
-                binding.button2.setOnClickListener {
-                    val intent = Intent(this@PayMethodActivity, PaidActivity::class.java).apply {
-
-                    }
-                    startActivity(intent)
-                }
-
-            }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pay_method)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
