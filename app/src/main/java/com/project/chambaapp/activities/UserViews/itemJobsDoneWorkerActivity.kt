d202c34c7e3044518283c1b646d5012db5bd80e7
brayanBH1 <76666927+bryanBH1@users.noreply.github.com>
2024-05-21 05:42:31 -0600

    Ventanas que me pidio el armandillo

---
+++ app/src/main/java/com/project/chambaapp/activities/UserViews/itemJobsDoneWorkerActivity.kt
@@ -0,0 +1,21 @@
+package com.project.chambaapp.activities.UserViews
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.project.chambaapp.R
+
+class itemJobsDoneWorkerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_item_jobs_done_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
